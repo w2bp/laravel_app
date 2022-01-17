3b76498b9fa632ab05a947d89e03d990319e7af2
wang <wbpsey@gmail.com>
2022-01-17 13:43:59 +0900

    modified 404 error page

---
+++ resources/lang/en/web.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'texts' => [
+        'page_not_found'                                        => 'This Page Is Not Found',
+        'go_back'                                               => 'Go Back',
+    ],
+];
--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,5 +1,221 @@
-@extends('errors::minimal')
+<!DOCTYPE html>
+<html>
+<head>
+    <title>404 | @lang('web.texts.page_not_found')</title>
+    <meta charset="utf-8">
+    <style>
+        body {
+            background-color: #000;
+            margin: 0px;
+            padding: 0px;
+            font-family: 'Roboto', sans-serif;
+        }
+        
+        .maincontainer {
+            position: relative;
+            top: -50px;
+            transform: scale(0.8);
+            background: url("{{ asset('images/errors/background.png') }}");
+            background-repeat: no-repeat;
+            background-position: center;
+            background-size: 700px 600px;
+            width: 800px;
+            height: 600px;
+            margin: 0px auto;
+            display: grid;
+        }
+        
+        .foregroundimg {
+            position: relative;
+            width: 100%;
+            top: -230px;
+            z-index: 5;
+        }
+        
+        .errorcode {
+            position: relative;
+            top: -180px;
+            font-family: 'Creepster', cursive;
+            color: white;
+            text-align: center;
+            font-size: 4em;
+            /* letter-spacing: 0.1em; */
+        }
+        
+        .errortext {
+            position: relative;
+            top: -220px;
+            color: #FBD130;
+            text-align: center;
+            text-transform: uppercase;
+            font-size: 1.5em;
+        }
+        
+        .errortext > a {
+            color: #fff;
+        }
+        
+        .bat {
+            opacity: 0;
+            position: relative;
+            transform-origin: center;
+            z-index: 3;
+        }
+        
+        .bat:nth-child(1) {
+            top: 380px;
+            left: 120px;
+            transform: scale(0.5);
+            animation: 13s 1s flyBat1 infinite linear;
+        }
+        
+        .bat:nth-child(2) {
+            top: 280px;
+            left: 80px;
+            transform: scale(0.3);
+            animation: 8s 4s flyBat2 infinite linear;
+        }
+        
+        .bat:nth-child(3) {
+            top: 200px;
+            left: 150px;
+            transform: scale(0.4);
+            animation: 12s 2s flyBat3 infinite linear;
+        }
+        
+        .body {
+            position: relative;
+            width: 50px;
+            top: 12px;
+        }
+        
+        .wing {
+            width: 150px;
+            position: relative;
+            transform-origin: right center;
+        }
+        
+        .leftwing {
+            left: 30px;
+            animation: 0.8s flapLeft infinite ease-in-out;
+        }
+        
+        .rightwing {
+            left: -180px;
+            transform: scaleX(-1);
+            animation: 0.8s flapRight infinite ease-in-out;
+        }
+        
+        @keyframes flapLeft {
+            0% {
+                transform: rotateZ(0);
+            }
+            50% {
+                transform: rotateZ(10deg) rotateY(40deg);
+            }
+            100% {
+                transform: rotateZ(0);
+            }
+        }
+        
+        @keyframes flapRight {
+            0% {
+                transform: scaleX(-1) rotateZ(0);
+            }
+            50% {
+                transform: scaleX(-1) rotateZ(10deg) rotateY(40deg);
+            }
+            100% {
+                transform: scaleX(-1) rotateZ(0);
+            }
+        }
+        
+        @keyframes flyBat1 {
+            0% {
+                opacity: 1;
+                transform: scale(0.5)
+            }
+            25% {
+                opacity: 1;
+                transform: scale(0.5) translate(-400px, -330px)
+            }
+            50% {
+                opacity: 1;
+                transform: scale(0.5) translate(400px, -800px)
+            }
+            75% {
+                opacity: 1;
+                transform: scale(0.5) translate(600px, 100px)
+            }
+            100% {
+                opacity: 1;
+                transform: scale(0.5) translate(100px, 300px)
+            }
+        }
+        
+        @keyframes flyBat2 {
+            0% {
+                opacity: 1;
+                transform: scale(0.3)
+            }
+            25% {
+                opacity: 1;
+                transform: scale(0.3) translate(200px, -330px)
+            }
+            50% {
+                opacity: 1;
+                transform: scale(0.3) translate(-300px, -800px)
+            }
+            75% {
+                opacity: 1;
+                transform: scale(0.3) translate(-400px, 100px)
+            }
+            100% {
+                opacity: 1;
+                transform: scale(0.3) translate(100px, 300px)
+            }
+        }
+        
+        @keyframes flyBat3 {
+            0% {
+                opacity: 1;
+                transform: scale(0.4)
+            }
+            25% {
+                opacity: 1;
+                transform: scale(0.4) translate(-350px, -330px)
+            }
+            50% {
+                opacity: 1;
+                transform: scale(0.4) translate(400px, -800px)
+            }
+            75% {
+                opacity: 1;
+                transform: scale(0.4) translate(-600px, 100px)
+            }
+            100% {
+                opacity: 1;
+                transform: scale(0.4) translate(100px, 300px)
+            }
+        }
+    </style>
+</head>
 
-@section('title', __('Not Found'))
-@section('code', '404')
-@section('message', __('Not Found'))
+<body>
+    <div class="maincontainer">
+        @for($i = 1; $i <= 3; $i++)
+        <div class="bat">
+            <img class="wing leftwing" src="{{ asset('images/errors/wing.png') }}">
+            <img class="body" src="{{ asset('images/errors/body.png') }}" alt="bat">
+            <img class="wing rightwing" src="{{ asset('images/errors/wing.png') }}">
+        </div>
+        @endfor
+        <img class="foregroundimg" src="{{ asset('images/errors/house.png') }}" alt="house">
+    </div>
+
+    <h1 class="errorcode">404</h1>
+    <div class="errortext">@lang('web.texts.page_not_found'). <a href="{{ \URL::previous() }}">@lang('web.texts.go_back')</a></div>
+
+</body>
+
+</html>
